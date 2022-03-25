13d33ef32afcc41776f6fef01f88ddd76d8d90b1
Null-Root <triascend94@gmail.com>
2022-03-25 17:08:42 +0800

    add: new example for functions

---
+++ examples/functions_example.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+using namespace std;
+
+
+#include "lib/datatypes/atomic_datatypes/datatype_void.h"
+display_stuffs
+#include "lib/parenthesis/open_parenthesis.h"
+#include "lib/parenthesis/close_parenthesis.h"
+#include "lib/parenthesis/open_brace.h"
+    cout << "Hello :)" << endl;
+    cout << "Hello :(" << endl;
+#include "lib/parenthesis/close_brace.h"
+
+
+#include "lib/datatypes/atomic_datatypes/datatype_int.h"
+sum_of_two_numbers
+#include "lib/parenthesis/open_parenthesis.h"
+#include "lib/datatypes/atomic_datatypes/datatype_int.h"
+num1,
+#include "lib/datatypes/atomic_datatypes/datatype_int.h"
+num2
+#include "lib/parenthesis/close_parenthesis.h"
+#include "lib/parenthesis/open_brace.h"
+    return
+    num1
+    #include "lib/operators/add.h"
+    num2
+    ;
+#include "lib/parenthesis/close_brace.h"
+
+
+
+#include "lib/datatypes/atomic_datatypes/datatype_int.h"
+main
+#include "lib/parenthesis/open_parenthesis.h"
+#include "lib/parenthesis/close_parenthesis.h"
+#include "lib/parenthesis/open_brace.h"
+    display_stuffs
+    #include "lib/parenthesis/open_parenthesis.h"
+    #include "lib/parenthesis/close_parenthesis.h"
+    ;
+
+    #include "lib/datatypes/atomic_datatypes/datatype_int.h"
+    sum
+    =
+    sum_of_two_numbers
+    #include "lib/parenthesis/open_parenthesis.h"
+    1,2
+    #include "lib/parenthesis/close_parenthesis.h"
+    ;
+
+    cout << "sum of 1 and 2 is " << sum << endl;
+
+    return 0;
+#include "lib/parenthesis/close_brace.h"
