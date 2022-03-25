9d790833d43a84666e75469868d0dd1004ea7426
Null-Root <triascend94@gmail.com>
2022-03-25 14:21:14 +0800

    add: new example for lib

---
+++ examples/example2.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+#include "lib/datatypes/atomic_datatypes/datatype_int.h"
+main
+#include "lib/parenthesis/open_parenthesis.h"
+#include "lib/parenthesis/close_parenthesis.h"
+
+#include "lib/parenthesis/open_brace.h"
+    #include "lib/datatypes/composite_datatypes/datatype_string.h"
+    msg1 = "hello";
+    
+    #include "lib/datatypes/composite_datatypes/datatype_string.h"
+    msg2 = "world";
+    
+    #include "lib/datatypes/atomic_datatypes/datatype_char.h"
+    msgch1 = ' ';
+    
+    #include "lib/datatypes/atomic_datatypes/datatype_char.h"
+    msgch2 = '!';
+    
+    #include "lib/datatypes/composite_datatypes/datatype_string.h"
+    final_msg = 
+    msg1
+    #include "lib/operators/add.h"
+    msgch1
+    #include "lib/operators/add.h"
+    msg2
+    #include "lib/operators/add.h"
+    msgch2;
+    
+
+    cout << final_msg << endl;
+    return 0;
+#include "lib/parenthesis/close_brace.h"
